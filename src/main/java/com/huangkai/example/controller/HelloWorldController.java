52ef0776360cb344d49a839abe2c09296bd8d0d6
huangkai <18778335525@163.com>
2019-01-27 22:21:24 +0800

    json接口开发

--- src/main/java/com/huangkai/example/controller/HelloWorldController.java
+++ src/main/java/com/huangkai/example/controller/HelloWorldController.java
@@ -1,12 +1,23 @@
 package com.huangkai.example.controller;
 
+import com.huangkai.example.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
-    public String index(){
-        return "Hello world";
+    public String index() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/getUser")
+    public User getUser() {
+        User user = new User();
+        user.setUsername("huang");
+        user.setPassword("admin123");
+        return user;
     }
 }
+
+
