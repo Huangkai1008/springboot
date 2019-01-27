9151e455a801975d5d17d72adc9981ad6c4151c3
huangkai <18778335525@163.com>
2019-01-27 19:04:20 +0800

    springboot项目初始化，第一个controller

---
+++ src/main/java/com/huangkai/example/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.huangkai.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello world";
+    }
+}
